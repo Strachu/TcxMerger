7644924daa0cb61622b1dacd88f4ab78d13630b1
Patryk Strach <Strachu@users.noreply.github.com>
2017-07-22 19:14:12 +0200

    Merging of TCX files.

---
+++ Src/CommandLineOptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine;
+using CommandLine.Text;
+using System.Linq;
+
+namespace TcxMerger
+{
+	public class CommandLineOptions
+	{
+		[Value(0, MetaName = "paths", Required = true, HelpText = "The paths of files to merge + destination path at the end. Wildcards (*) are supported.")]
+		public IEnumerable<string> Paths { get; set; }
+
+		public IEnumerable<string> SourceFiles
+		{
+			get
+			{
+				return Paths.Reverse().Skip(1).Reverse();
+			}
+		}
+
+		public string DestinationFilePath
+		{
+			get
+			{
+				return Paths.Last();
+			}
+		}
+
+		public static string GetHelpText(ParserResult<CommandLineOptions> parsingResult)
+		{
+			var exeName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+
+			var help = HelpText.AutoBuild(parsingResult);
+
+			help.AddPreOptionsLine(" ");
+			help.AddPreOptionsLine($"Usage: {exeName} [options] source_file1_path, source_file2_path, ..., destination_path");
+
+			return help.ToString();
+		}
+	}
+}
---
+++ Src/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommandLine;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TcxMerger
+{
+	public class MainClass
+	{
+		public static int Main(string[] args)
+		{
+			var parsingResult = new Parser(x => x.HelpWriter = null).ParseArguments<CommandLineOptions>(args);
+
+			return parsingResult.MapResult(x => Run(x).Result, errors =>
+			{
+				Console.WriteLine(CommandLineOptions.GetHelpText(parsingResult));
+				return 1;
+			});
+		}
+
+		private static async Task<int> Run(CommandLineOptions options)
+		{
+			var filesToMerge = GetFilesToMerge(options.SourceFiles).ToList();
+			if(!filesToMerge.Any())
+			{
+				Console.WriteLine("No files to merge. Exiting...");
+				return 0;
+			}
+
+			Console.WriteLine($"Got {filesToMerge.Count} files to merge into {options.DestinationFilePath}.");
+
+			var mergedXml = MergeFiles(filesToMerge);
+
+			Console.WriteLine($"Writing the result to {options.DestinationFilePath}.");
+
+			Directory.CreateDirectory(Path.GetDirectoryName(options.DestinationFilePath));
+			mergedXml.Save(options.DestinationFilePath, SaveOptions.DisableFormatting);
+
+			return 0;
+		}
+
+		// On Winshit a command prompt opposing to a bash does not expand the wildcard.
+		private static IEnumerable<string> GetFilesToMerge(IEnumerable<string> patterns)
+		{
+			foreach(var pattern in patterns)
+			{
+				var isFile = !File.GetAttributes(pattern).HasFlag(FileAttributes.Directory);
+				if(isFile)
+				{
+					yield return pattern;
+				}
+				else
+				{
+					foreach(var file in Directory.EnumerateFiles(pattern))
+					{
+						yield return file;
+					}
+				}
+			}
+		}
+
+		private static XDocument MergeFiles(IEnumerable<string> filesToMerge)
+		{
+			var destinationActivitiesElement = ReadActivitiesTag(filesToMerge.First());
+
+			foreach(var nextFile in filesToMerge.Skip(1))
+			{
+				var fileActiviesElement = ReadActivitiesTag(nextFile);
+
+				destinationActivitiesElement.Add(fileActiviesElement);
+			}
+
+			return destinationActivitiesElement.Document;
+		}
+
+		private static XElement ReadActivitiesTag(string filePath)
+		{
+			Console.WriteLine($"Loading {filePath}...");
+			
+			var xmlDocument = XDocument.Load(filePath);
+
+			return xmlDocument.Root.Descendants().Single(x => x.Name.LocalName == "Activities");
+		}
+	}
+}
